fb250415f4133aeecf42775eea9eed893e895a09
Alia Peterson <alia@pnwebs.com>
2021-03-02 17:28:57 -0700

    Setup file structures

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './App.css'
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  componentDidMount = () => {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>hello</h1>
+      </div>
+    )
+  }
+}
