be33e739dae8ad2bb15af2eea62d30b7f7d6bfea
Alia Peterson <alia@pnwebs.com>
2021-03-02 21:34:05 -0700

    Add components into their own file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,12 +7,23 @@ import Header from '../Header/Header'
 import Form from '../Form/Form'
 import Recipe from '../Recipe/Recipe'
 import Question from '../Question/Question'
-import fetchAPI from '../fetchAPI'
+import fetchAPI from '../../fetchAPI'
 import './App.css'
 
 export default function App() {
   const [recipe, setRecipe] = useState([])
   const [trivia, setTrivia] = useState([])
+
+  const populateInformation = () => {
+    const recipeInfo = fetchAPI.getRecipe()
+    const triviaInfo = fetchAPI.getTrivia()
+
+    Promise.all([recipeInfo, triviaInfo])
+      .then(response => {
+        setRecipe(response[0])
+        setTrivia(response[1])
+      })
+  }
 
   return (
     <Router>
