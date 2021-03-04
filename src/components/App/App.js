2c34c0e5c855f3a1b5f24a6a541a0b51301cbc48
Alia Peterson <alia@pnwebs.com>
2021-03-03 21:44:53 -0700

    Update how form information is handled

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,29 +17,19 @@ export default function App() {
   const [currentBeverage, setCurrentBeverage] = useState({})
   const [trivia, setTrivia] = useState([])
 
-  const populateInformation = (event) => {
-    event.preventDefault()
+  const populateRecipe = (event) => {
+    console.log('success!');
     const recipeInfo = fetchAPI.getRecipe()
-    const triviaInfo = fetchAPI.getTrivia()
     const number = Math.floor(Math.random() * Math.floor(recipes.length))
 
     setRecipes(recipeInfo)
-    setTrivia(triviaInfo)
-    setCurrentBeverage(recipes[0])
-    console.log('recipe', recipes[0])
-
-    // Promise.all([recipeInfo, triviaInfo])
-    //   .then(response => {
-    //     setRecipes(response[0])
-    //     setTrivia(response[1])
-    //   })
-    //   .then(() => {
-    //     setCurrentBeverage(recipes[number])
-    //   })
+    setCurrentBeverage(recipes[number])
   }
 
-  // useEffect(() => {
-  // }, [])
+  const populateTrivia = () => {
+    const triviaInfo = fetchAPI.getTrivia()
+    setTrivia(triviaInfo)
+  }
 
   return (
     <Router>
@@ -49,7 +39,10 @@ export default function App() {
           <Route
             exact path='/'
             render={() => {
-              return <Form submit={populateInformation}/>
+              return <Form
+                populateRecipe={populateRecipe}
+                populateTrivia={populateTrivia}
+                />
             }}
             />
           <Route
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Form.css'
 
-export default function Form({ submit }) {
+export default function Form({ populateRecipe, populateTrivia }) {
   const [drinkBase, setDrinkBase] = useState('')
   const [triviaDifficulty, setTriviaDifficulty] = useState([])
 
@@ -10,19 +10,35 @@ export default function Form({ submit }) {
     <article className='form-container'>
       <form className='form'>
         <label htmlFor='dropdown-drinks'>Drink Base:</label>
-        <select className='dropdown' onChange={setDrinkBase} id='dropdown-drinks'>
+        <select
+          onChange={() => {
+            setDrinkBase()
+            populateRecipe()
+          }}
+          className='dropdown'
+          id='dropdown-drinks'
+          >
           <option value='vodka'>Vodka</option>
           <option value='tequila'>Tequila</option>
           <option value='gin'>Gin</option>
         </select>
         <label htmlFor='dropdown-trivia'>Trivia Difficulty:</label>
-        <select className='dropdown' onChange={setTriviaDifficulty} id='dropdown-trivia'>
+        <select
+          onChange={() => {
+            setTriviaDifficulty()
+            populateTrivia()
+          }}
+          className='dropdown'
+          id='dropdown-trivia'
+          >
           <option value='easy'>Easy</option>
           <option value='medium'>Medium</option>
           <option value='hard'>Hard</option>
         </select>
         <Link to='/trivia'>
-          <button className='button' onClick={submit}>Submit</button>
+          <button className='button'>
+            Submit
+          </button>
         </Link>
       </form>
     </article>
