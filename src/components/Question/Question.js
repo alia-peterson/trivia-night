6910b272b578ef6cff100aa7890698c786fbe98c
Alia Peterson <alia@pnwebs.com>
2021-03-04 17:07:42 -0700

    Update question component to remove all that class nonsense

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,98 +1,39 @@
-import React, { Component } from 'react'
+import React, from 'react'
 import './Question.css'
 
-export default class Question extends Component {
-  constructor() {
-    super()
-    this.state = {
-      allQuestions: [],
-      category: '',
-      questionNumber: 1,
-      currentQuestion: {}
-    }
-  }
-
-  // let questionNumber = 1
-  // const triviaCategory = questions[0].category
-  // const currentQuestion = questions[questionNumber]
-  // const possibleAnswers = [currentQuestion.correct_answer, ...currentQuestion.incorrect_answers]
-  // const answerList = possibleAnswers.map((answer, index) => {
-  //   if (index === 0) {
-  //     return <button
-  //       key={index}
-  //       onClick={answerQuestion}
-  //       className='button-trivia correct'
-  //       >
-  //       {answer}
-  //     </button>
-  //   } else {
-  //     return <button
-  //       key={index}
-  //       onClick={answerQuestion}
-  //       className='button-trivia incorrect'
-  //       >
-  //       {answer}
-  //     </button>
-  //   }
-  // })
-
-  randomizeAnswers = () => {
-
-  }
-
-  updateQuestion = () => {
-
-  }
-
-  componentDidMount = () => {
-    const storedInformation = JSON.parse(localStorage.getItem('triviology-info'))
-    const storedTrivia = JSON.parse(localStorage.getItem('triviology-trivia'))
-    console.log(storedInformation);
-    console.log(storedTrivia);
-
-    if (storedInformation && storedTrivia) {
-      this.setState({
-        allQuestions: storedInformation.questions,
-        category: storedInformation.trivia[0].category,
-        currentQuestion: storedInformation.trivia[0]
-      })
-
-    } else {
-      this.setState({
-        allQuestions: this.props.questions,
-        category: this.props.questions[0].category,
-        currentQuestion: this.props.questions[0]
-      })
-      localStorage.setItem('triviology-trivia', JSON.stringify(this.state))
-    }
-    this.randomizeAnswers()
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>Category: {this.state.category}</h2>
-        <article className='trivia-container' id={this.state.questionNumber}>
-          <h2>Question {this.state.questionNumber}:</h2>
-          <p>{this.state.currentQuestion.question}</p>
-          <div>
-            <button
-              onClick={this.updateQuestion}
-              className='correct'
-              >
-              {this.state.currentQuestion.correct_answer}
-            </button>
-
-          </div>
+export default function Question({ question, answerQuestion }) {
+  const incorrectAnswers = question.incorrect_answers.map(answer => {
+    return <button
+      onClick={answerQuestion}
+      className='incorrect'
+      >
+      {answer}
+    </button>
+  })
+  
+  return (
+    <div>
+      <h2>Category: {question.category}</h2>
+      <article className='trivia-container' id={}>
+        <h2>Question:</h2>
+        <p>{question.question}</p>
+        <div>
           <button
-            className='button'
-            onClick={this.updateQuestion}
-            disabled
+            onClick={answerQuestion}
+            className='correct'
             >
-            Next Question >
+            {question.correct_answer}
           </button>
-        </article>
-      </div>
-    )
-  }
+          {incorrectAnswers}
+        </div>
+        <button
+          className='button'
+          onClick={this.updateQuestion}
+          disabled
+          >
+          Next Question >
+        </button>
+      </article>
+    </div>
+  )
 }
