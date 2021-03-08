2a7a592fc3bbcd44b204ce171a9b42805d9fb03a
Alia Peterson <alia@pnwebs.com>
2021-03-08 15:36:43 -0700

    Add react hook to avoid reloading component when classname is updated

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Question.css'
 
 export default function Question({ question, answered, number, score, answerQuestion, restart, newGame }) {
-  let possibleAnswers
+  const [possibleAnswers, setPossibleAnswers] = useState([])
 
   if (!question.category) {
     question = JSON.parse(localStorage.getItem('triviology-info')).currentQuestion
   }
 
-  if (question.correct_answer) {
-    const incorrectAnswers = question.incorrect_answers.map((answer, index) => {
+  const allAnswers = possibleAnswers.map((answer, index) => {
+    if (answer === question.correct_answer) {
+      return <button
+        key={index}
+        onClick={answerQuestion}
+        className='button correct'
+        dangerouslySetInnerHTML={{__html: answer}}
+        >
+      </button>
+    } else {
       return <button
         key={index}
         onClick={answerQuestion}
@@ -17,33 +25,25 @@ export default function Question({ question, answered, number, score, answerQues
         dangerouslySetInnerHTML={{__html: answer}}
         >
       </button>
-    })
-    // dangerouslySetInnerHTML is a solution i'm tolerating for now as i can't
-    // find a better solution for interpreting html characters
+    }
+  })
 
-    const correctAnswer = (
-      <button
-        key={3}
-        onClick={answerQuestion}
-        className='button correct'
-        dangerouslySetInnerHTML={{__html: question.correct_answer}}
-        >
-      </button>
-    )
-
-    const random = Math.floor(Math.random() * Math.floor(incorrectAnswers.length))
-    possibleAnswers = incorrectAnswers
-    possibleAnswers.splice(random, 0, correctAnswer)
-  }
+  useEffect(() => {
+    const answers = question.incorrect_answers
+    const random = Math.floor(Math.random() * Math.floor(answers.length))
+    answers.splice(random, 0, question.correct_answer)
+    console.log(answers);
+    setPossibleAnswers(answers)
+  }, [question])
 
   return (
     <div>
       <h2>Category: {question.category}</h2>
       <article className={`trivia-container ${answered && 'show-results'}`} id={question.id}>
         {number > 10 ?
-          <div>
-            <h2>Your Score:</h2>
-            <p>You got {score} out of 10 correct!</p>
+          <>
+            <h3>Your Score:</h3>
+            <p className='trivia-score'>You got {score} out of 10 correct!</p>
             <div className='button-container'>
               <button
                 className='button'
@@ -58,14 +58,14 @@ export default function Question({ question, answered, number, score, answerQues
                 New Game
               </button>
             </div>
-          </div> :
-          <div>
+          </> :
+          <>
             <h2>Question {question.id} of 10:</h2>
             <p dangerouslySetInnerHTML={{__html: question.question}} />
             <div className='button-container'>
-              {possibleAnswers}
+              {allAnswers}
             </div>
-          </div>
+          </>
         }
       </article>
     </div>
