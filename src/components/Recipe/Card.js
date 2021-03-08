7ac14c5f0a75b1a79b61c42dffea25300a52f37f
Alia Peterson <alia@pnwebs.com>
2021-03-08 14:06:56 -0700

    Move recipe information into card component to be reusable

--- src/components/Recipe/Card.js
+++ src/components/Recipe/Card.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import iconFilled from '../../assets/icon-heart-filled.png'
+import iconOutline from '../../assets/icon-heart-outline.png'
+import './Card.css'
 
-export default function Card({ drinkName, drinkImage, favoriteIcon, iconAltText, toggleFavorite, ingredients, instructions }) {
+export default function Card({ recipe, isFavorite, toggleFavorite }) {
+  const instructions = recipe.strInstructions
+  const ingredientDescriptions = Object.values(recipe)
+  const ingredientProperties = Object.keys(recipe)
+
+  let favoriteIcon
+  let iconAltText
+
+  if (isFavorite) {
+    favoriteIcon = iconFilled
+    iconAltText = 'Favorite icon filled'
+  } else {
+    favoriteIcon = iconOutline
+    iconAltText = 'Favorite icon unfilled'
+  }
+
+  let ingredientNumber = 1
+  const ingredientsList = ingredientProperties.reduce((acc, curr, index, array) => {
+
+    if (curr === `strIngredient${ingredientNumber}` &&
+                  ingredientDescriptions[index]) {
+      const measurement = array.find(item => {
+        return item === `strMeasure${ingredientNumber}`
+      })
+
+      acc.push(recipe[curr] + ': ' + recipe[measurement])
+      ingredientNumber += 1
+    }
+
+    return acc
+  }, [])
+
+  const ingredients = ingredientsList.map((ing, index) => {
+    return <li key={index}>{ing}</li>
+  })
+
   return (
-    <div>
+    <>
       <img
         className='fav-icon'
         src={favoriteIcon}
@@ -11,14 +49,14 @@ export default function Card({ drinkName, drinkImage, favoriteIcon, iconAltText,
         />
       <div className='recipe-inst-container'>
         <div>
-          <h3 className='recipe-name'>{drinkName}</h3>
+          <h3 className='recipe-name'>{recipe.strDrink}</h3>
           <h4 className='recipe-heading'>Ingredients:</h4>
           <ul className='recipe-ingr'>{ingredients}</ul>
         </div>
-        <img className='recipe-image' src={drinkImage} alt=''/>
+        <img className='recipe-image' src={recipe.strDrinkThumb} alt=''/>
       </div>
       <h4 className='recipe-heading'>Instructions:</h4>
       <p className='recipe-inst'>{instructions}</p>
-    </div>
+    </>
   )
 }
--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,58 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Card from './Card'
-import iconFilled from '../../assets/icon-heart-filled.png'
-import iconOutline from '../../assets/icon-heart-outline.png'
 import './Recipe.css'
 
 export default function Recipe({ recipe, newBeverage, triviaEnabled, isFavorite, toggleFavorite }) {
-  const instructions = recipe.strInstructions
-  const ingredientDescriptions = Object.values(recipe)
-  const ingredientProperties = Object.keys(recipe)
-
-  let favoriteIcon
-  let iconAltText
-
-  if (isFavorite) {
-    favoriteIcon = iconFilled
-    iconAltText = 'Favorite icon filled'
-  } else {
-    favoriteIcon = iconOutline
-    iconAltText = 'Favorite icon unfilled'
-  }
-
-  let ingredientNumber = 1
-  const ingredientsList = ingredientProperties.reduce((acc, curr, index, array) => {
-
-    if (curr === `strIngredient${ingredientNumber}` &&
-                  ingredientDescriptions[index]) {
-      const measurement = array.find(item => {
-        return item === `strMeasure${ingredientNumber}`
-      })
-
-      acc.push(recipe[curr] + ': ' + recipe[measurement])
-      ingredientNumber += 1
-    }
-
-    return acc
-  }, [])
-
-  const ingredients = ingredientsList.map((ing, index) => {
-    return <li key={index}>{ing}</li>
-  })
-
   return (
     <div>
       <h2>Beverage Recipe:</h2>
       <article className='recipe-container' id={recipe.idDrink}>
         <Card
-          drinkName={recipe.strDrink}
-          drinkImage={recipe.strDrinkThumb}
-          favoriteIcon={favoriteIcon}
-          iconAltText={iconAltText}
+          recipe={recipe}
+          isFavorite={isFavorite}
           toggleFavorite={toggleFavorite}
-          ingredients={ingredients}
-          instructions={instructions}
           />
         <button
           className='button'
